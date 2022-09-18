33ef01ac90b2dda4b48b1060f723b6846faf2219
Tiago Pala <tiago_pala@outlook.com>
2022-09-18 00:04:34 -0300

    Adição security groups habilitando ssh

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -13,12 +13,32 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+# EC2 Instances
 resource "aws_instance" "dev" {
   count         = 1
   ami           = "ami-0aca10934d525a6f0"
   instance_type = "t2.micro"
-  key_name      = "terraformKP"
+  key_name      = "terraform-kp"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     "Name" = "dev-${count.index}"
+  }
+}
+
+# EC2 Security Groups
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
